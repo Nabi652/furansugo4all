1453910bd0826b16f7cf889d7c3134a12ccf5421
nabila hajou <nab.ikk13@gmail.com>
2021-10-28 00:08:15 +0200

    changement du site avec ajout exo feminin/masculin

---
+++ exerciceFemMasc1.js
@@ -0,0 +1,35 @@
+let reponse1 = document.getElementById("reponse1");
+
+reponse1.addEventListener("click", () => {
+  reponse1.style.border = "1px solid green";
+  reponse1.style.textAlign = "center";
+  reponse1.style.margin = "auto";
+  reponse1.style.transition = "1s";
+  reponse1.innerHTML = `Une chanteuse <br> Une actrice </br> Une danseuse </br> Une boulangère </br> Une patissière </br> Une policière </br> Une conductrice </br> Une présentatrice`;
+  reponse1.style.transition = "1s";
+  reponse1.style.marginBottom = "30px";
+});
+
+let reponse2 = document.getElementById("reponse2");
+
+reponse2.addEventListener("click", () => {
+  reponse2.style.border = "1px solid green";
+  reponse2.style.textAlign = "center";
+  reponse2.style.margin = "auto";
+  reponse2.style.transition = "1s";
+  reponse2.innerHTML = `Elle est gentille <br> Elle a une soeur </br> Elle a deux chiennes</br> Elle est directrice </br>Elle est triste </br>Elle est heureuse </br> Elle a une chatte </br>Elle est plombière`;
+  reponse2.style.transition = "1s";
+  reponse2.style.marginBottom = "30px";
+});
+
+let reponse3 = document.getElementById("reponse3");
+
+reponse3.addEventListener("click", () => {
+  reponse3.style.border = "1px solid green";
+  reponse3.style.textAlign = "center";
+  reponse3.style.margin = "auto";
+  reponse3.style.transition = "1s";
+  reponse3.innerHTML = `Elle est gentille <br> Elle a une soeur </br> Elle a deux chiennes</br> Elle est directrice </br>Elle est triste </br>Elle est heureuse </br> Elle a une chatte </br>Elle est plombière`;
+  reponse3.style.transition = "1s";
+  reponse3.style.marginBottom = "30px";
+});
