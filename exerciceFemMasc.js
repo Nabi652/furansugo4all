ceec272054d74deaa5c11417fb9f76841f1a4dbd
nabila hajou <nab.ikk13@gmail.com>
2021-11-04 20:42:34 +0100

    added details for exercises of masculinfeminin.html file

--- exerciceFemMasc.js
+++ exerciceFemMasc.js
@@ -4,7 +4,7 @@ reponse1.addEventListener("click", () => {
   reponse1.style.border = "1px solid green";
   reponse1.style.textAlign = "center";
   reponse1.style.margin = "auto";
-  reponse1.innerHTML = `Une chanteuse <br> Une actrice </br> Une danseuse </br> Une boulangère </br> Une patissière </br> Une policière </br> Une conductrice </br> Une présentatrice`;
+  reponse1.innerHTML = `a- Une chanteuse <br> b- Une actrice </br> c- Une danseuse </br> d- Une boulangère </br> d- Une patissière </br> e- Une policière </br> f- Une conductrice </br> g- Une présentatrice`;
   reponse1.style.marginBottom = "30px";
   reponse1.style.height = "auto";
   reponse1.style.padding = "10px";
@@ -16,8 +16,7 @@ reponse2.addEventListener("click", () => {
   reponse2.style.border = "1px solid green";
   reponse2.style.textAlign = "center";
   reponse2.style.margin = "auto";
-
-  reponse2.innerHTML = `Elle est gentille <br> Elle a une soeur </br> Elle a deux chiennes</br> Elle est directrice </br>Elle est triste </br>Elle est heureuse </br> Elle a une chatte </br>Elle est plombière`;
+  reponse2.innerHTML = `a- Elle est gentille <br> b- Elle a une soeur </br> c- Elle a deux chiennes</br> d- Elle est directrice </br>e- Elle est triste </br>f- Elle est heureuse </br> g- Elle a une chatte </br>h- Elle est plombière`;
   reponse2.style.marginBottom = "30px";
   reponse2.style.height = "auto";
   reponse2.style.padding = "10px";
@@ -29,7 +28,7 @@ reponse3.addEventListener("click", () => {
   reponse3.style.border = "1px solid green";
   reponse3.style.textAlign = "center";
   reponse3.style.margin = "auto";
-  reponse3.innerHTML = `Il est amoureux <br> Il veut un fils </br> Il est cuisinier</br> C'est un homme </br> Il est intelligent </br>Il est mignon </br> Il est patient </br>Il est beau`;
+  reponse3.innerHTML = `a- Il est amoureux <br> b- Il veut un fils </br> c- Il est cuisinier</br> d- C'est un homme </br> e- Il est intelligent </br>f- Il est mignon </br> g- Il est patient </br> h- Il est beau`;
   reponse3.style.marginBottom = "30px";
   reponse3.style.height = "auto";
   reponse3.style.padding = "10px";
@@ -65,10 +64,10 @@ reponse6.addEventListener("click", () => {
   reponse6.style.border = "1px solid green";
   reponse6.style.textAlign = "center";
   reponse6.style.margin = "auto";
-  reponse6.innerHTML = `1- C'est une magicienne. Elle habite dans la forêt. Elle est petite et vilaine. Elle a les cheveux longs et noirs. Elle est très rusée et rapide. Elle déteste les enfants et elle préfère rester seule. </br>
-  2- Il est policier. Il travaille toute la journée. Il doit arrêter les criminels et les voleurs. Son travail n'est pas facile mais son travail est intéressant. </br>
-  3- Elle est maîtresse d'école. Elle est grande et grosse. Elle est très gentille et patiente. Tous ses élèves aiment beaucoup cette maîtresse d'école. </br>
-  4- Il est pâtissier. Il prépare de très bons gâteaux. Son gâteau préféré est au chocolat. Il est très doué et il a beaucoup d'imagination pour faire ses gâteaux.  `;
+  reponse6.innerHTML = `a- C'est une magicienne. Elle habite dans la forêt. Elle est petite et vilaine. Elle a les cheveux longs et noirs. Elle est très rusée et rapide. Elle déteste les enfants et elle préfère rester seule. </br>
+  b- Il est policier. Il travaille toute la journée. Il doit arrêter les criminels et les voleurs. Son travail n'est pas facile mais son travail est intéressant. </br>
+  c- Elle est maîtresse d'école. Elle est grande et grosse. Elle est très gentille et patiente. Tous ses élèves aiment beaucoup cette maîtresse d'école. </br>
+  d- Il est pâtissier. Il prépare de très bons gâteaux. Son gâteau préféré est au chocolat. Il est très doué et il a beaucoup d'imagination pour faire ses gâteaux.  `;
   reponse6.style.marginBottom = "30px";
   reponse6.style.height = "auto";
   reponse6.style.padding = "10px";
