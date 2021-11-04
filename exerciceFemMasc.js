130067bd3f98551a65eed02336dc11657e9742c0
nabila hajou <nab.ikk13@gmail.com>
2021-11-04 19:00:45 +0100

    added <p> to <div> cliquer pour voir la reponse from masculinfeminin html file

--- exerciceFemMasc.js
+++ exerciceFemMasc.js
@@ -8,6 +8,7 @@ reponse1.addEventListener("click", () => {
   reponse1.innerHTML = `Une chanteuse <br> Une actrice </br> Une danseuse </br> Une boulangère </br> Une patissière </br> Une policière </br> Une conductrice </br> Une présentatrice`;
   reponse1.style.transition = "1s";
   reponse1.style.marginBottom = "30px";
+  reponse1.style.height = "auto";
 });
 
 let reponse2 = document.getElementById("reponse2");
@@ -20,6 +21,7 @@ reponse2.addEventListener("click", () => {
   reponse2.innerHTML = `Elle est gentille <br> Elle a une soeur </br> Elle a deux chiennes</br> Elle est directrice </br>Elle est triste </br>Elle est heureuse </br> Elle a une chatte </br>Elle est plombière`;
   reponse2.style.transition = "1s";
   reponse2.style.marginBottom = "30px";
+  reponse2.style.height = "auto";
 });
 
 let reponse3 = document.getElementById("reponse3");
@@ -32,6 +34,7 @@ reponse3.addEventListener("click", () => {
   reponse3.innerHTML = `Elle est gentille <br> Elle a une soeur </br> Elle a deux chiennes</br> Elle est directrice </br>Elle est triste </br>Elle est heureuse </br> Elle a une chatte </br>Elle est plombière`;
   reponse3.style.transition = "1s";
   reponse3.style.marginBottom = "30px";
+  reponse3.style.height = "auto";
 });
 
 let reponse4 = document.getElementById("reponse4");
@@ -44,6 +47,7 @@ reponse4.addEventListener("click", () => {
   reponse4.innerHTML = `Mon amie est peintre. Elle travaille à Paris depuis 2 ans. Elle participe à des expositions et elle connait beaucoup d'artistes. Le mois prochain, mon amie va aller à Londres pour exposer ses oeuvres. Elle est très contente mais elle est aussi un peu stressée parce qu'elle a peur que tout se passe mal. Pour préparer cette exposition, elle a besoin de travailler plus que d'habitude. Sa famille est très inquiète pour elle parce qu'elle est toujours fatiguée et elle est de mauvaise humeur. Je suis sûre que tout va bien se passer, elle doit rester positive et profiter de ce moment!`;
   reponse4.style.transition = "1s";
   reponse4.style.marginBottom = "30px";
+  reponse4.style.height = "auto";
 });
 
 let reponse5 = document.getElementById("reponse5");
@@ -56,4 +60,5 @@ reponse5.addEventListener("click", () => {
   reponse5.innerHTML = `C'est un chanteur, très populaire et connu dans le monde entier. Il fait des concerts partout et il a beaucoup de fans internationaux. Tout le monde aime sa jolie voix mais aussi sa personnalité et son sens de l'humour. Parfois, il chante avec son père, qui est aussi un grand chanteur. Il a arrêté sa carrière quand son fils est né. Ce chanteur aime son métier et il veut continuer jusqu'au bout. `;
   reponse5.style.transition = "1s";
   reponse5.style.marginBottom = "30px";
+  reponse5.style.height = "auto";
 });
